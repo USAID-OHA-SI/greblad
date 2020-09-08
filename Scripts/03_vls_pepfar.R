a1b116da6781aa4867cd7ce50c3225a8dcdb8457
Aaron Chafetz <achafetz@usaid.gov>
2020-09-08 11:01:57 -0400

    include both program vls and proxy vls

--- Scripts/03_vls_pepfar.R
+++ Scripts/03_vls_pepfar.R
@@ -3,7 +3,7 @@
 ##  PURPOSE: MER VLS
 ##  LICENCE: MIT
 ##  DATE:    2020-09-03
-##  UPDATE:  
+##  UPDATE:  2020-09-08
 
 
 # DEPENDENCIES ------------------------------------------------------------
@@ -52,24 +52,72 @@ library(ICPIutilities)
   #filter to necessary variables
     df_vl <- df_mer %>% 
       filter(indicator == "TX_PVLS",
-             standardizeddisaggregate == "Total Numerator",
+             standardizeddisaggregate  == "Total Numerator",
              fiscal_year == 2019)
-  
+    
   #summarize to FY19Q4
     df_vl <- df_vl %>% 
       group_by(countryname) %>% 
       summarise(tx_pvls = sum(qtr4, na.rm = TRUE)) %>% 
       ungroup() %>% 
       mutate(tx_pvls_year = "FY19Q4")
-  
+    
   #calc VLS - PVLS_N FY19Q4
     df_vls <- df_vl %>% 
       full_join(df_nat) %>%
       mutate(vls_pepfar = tx_pvls / plhiv_pepfar)
-      
+    
+    
+  #filter to necessary variables
+    df_vl_proxy <- df_mer %>% 
+      filter(indicator %in% c("TX_CURR", "TX_PVLS"),
+             standardizeddisaggregate %in% c("Total Numerator", "Total Denominator"))
+  
+  #add D to PVLS denom
+    df_vl_proxy <- df_vl_proxy %>% 
+      mutate(indicator = ifelse(numeratordenom == "D", paste0(indicator, "_D"), indicator))
+    
+  #keep to just quarterly results to create VLC
+    df_vl_proxy <- df_vl_proxy %>% 
+      group_by(countryname, fundingagency, fiscal_year, indicator) %>% 
+      summarise(across(starts_with("qtr"), sum, na.rm = TRUE)) %>% 
+      ungroup()
+    
+  #reshape long to setup VLC calc over quarters
+    df_vl_proxy <- df_vl_proxy %>% 
+      reshape_msd(clean = TRUE) %>% 
+      select(-period_type) %>% 
+      spread(indicator, val)
+    
+  #calc prior TX_CURR for ease
+    df_vl_proxy <- df_vl_proxy %>%
+      group_by(countryname, fundingagency) %>%
+      mutate(TX_CURR_2prior = lag(TX_CURR, 2, order_by = period)) %>% 
+      ungroup()
+    
+  #calc VLC + VLS (due to Q3 issues in ZAF, need to use Q2 data)
+    df_vl_proxy <- df_vl_proxy %>% 
+      mutate(VLC = TX_PVLS_D/TX_CURR_2prior,
+             VLS_proxy = (TX_PVLS/TX_PVLS_D)*VLC) %>% 
+      filter(period == ifelse(countryname == "South Africa", "FY20Q2", max(period))) %>% 
+      select(-period)
+    
+  #ensure every ctry has each agency (for USAID filtering) and then calc PEPFAR totals
+    df_vl_proxy <- df_vl_proxy %>%
+      complete(fundingagency, nesting(countryname)) %>% 
+      group_by(countryname) %>% 
+      mutate(VLC_pepfar = sum(TX_PVLS_D, na.rm = TRUE)/sum(TX_CURR_2prior, na.rm = TRUE),
+             VLS_proxy_pepfar = (sum(TX_PVLS, na.rm = TRUE)/sum(TX_PVLS_D, na.rm = TRUE))*VLC) %>%
+      ungroup() %>% 
+      filter(fundingagency == "USAID") %>% 
+      rename_with(~ paste0(., "_usaid"), c(VLC, VLS_proxy)) %>% 
+      rename_all(tolower) %>% 
+      select(countryname, starts_with("vls"))
+    
 
-  #add iso code in for merging
-    df_vls <- df_vls %>% 
+  #merge, adding iso code
+    df_vls <- df_vls %>%
+      full_join(df_vl_proxy) %>% 
       mutate(iso = countrycode(countryname, "country.name", "iso3c", warn = FALSE)) %>% 
       relocate(iso, .after = "countryname")
     
