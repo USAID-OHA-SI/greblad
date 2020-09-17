a99fedc0799349384dd957921a07debff547127f
Aaron Chafetz <achafetz@usaid.gov>
2020-09-17 18:47:44 -0400

    add FY20 USAID and PEPFAR budgets to FY21

--- Scripts/01_budget.R
+++ Scripts/01_budget.R
@@ -20,30 +20,40 @@
   sheet_id <- "15REkQ2kNIgYR3LQ8k7WnQYmXBY4UmZpaRNwRQqI-V7I"
   gs4_auth()
   df_copmatrix <- read_sheet(sheet_id)
+  df_copmatrix19 <- read_sheet(sheet_id, sheet = "2019 Cop matrix")
+  
+  sheet_id_gh <- "1rzlWJgVW1fpQ3CLS26fwnDcq4PKsVS-uqEZBk67Ojy8"
+  df_ghbudget <- read_sheet(sheet_id_gh)
 
 
 # MUNGE -------------------------------------------------------------------
 
   #make names usable  
-    df_copmatrix <- clean_names(df_copmatrix)
+    df_copmatrix <- df_copmatrix %>% 
+      bind_rows(df_copmatrix19) %>% 
+      clean_names()
 
   #create planning lvl w/o M&O and w/ + w/o supply chain; aggregate to OU x agency level
     df_copmatrix_agg <- df_copmatrix %>%
       filter(record_type != "Management and Operations") %>% 
-      mutate(total_planned_funding_sans_ghsc = case_when(str_detect(mechanism_name, "GHSC", negate = TRUE) ~ total_planned_funding)) %>% 
-      group_by(operating_unit, funding_agency) %>% 
-      summarise(cop20_budget = sum(total_planned_funding, na.rm = TRUE),
-                cop20_budget_sans_ghsc = sum(total_planned_funding_sans_ghsc, na.rm = TRUE)) %>% 
+      mutate(total_planned_funding_sans_ghsc = case_when(str_detect(mechanism_name, "GHSC", negate = TRUE) ~ total_planned_funding),
+             planning_cycle = str_remove_all(planning_cycle, "^20| COP") %>% paste0("cop", .)) %>% 
+      group_by(operating_unit, funding_agency, planning_cycle) %>% 
+      summarise(budget = sum(total_planned_funding, na.rm = TRUE),
+                budget_sans_ghsc = sum(total_planned_funding_sans_ghsc, na.rm = TRUE)) %>% 
       ungroup()
 
   #USAID share of PEPFAR budget by OU
     df_budget <- df_copmatrix_agg %>% 
       group_by(operating_unit) %>% 
-      mutate(cop20_share = cop20_budget/sum(cop20_budget, na.rm = TRUE),
-             cop20_share_sans_ghsc = cop20_budget_sans_ghsc/sum(cop20_budget_sans_ghsc, na.rm = TRUE)) %>% 
+      mutate(share = budget/sum(budget, na.rm = TRUE),
+             share_sans_ghsc = budget_sans_ghsc/sum(budget_sans_ghsc, na.rm = TRUE)) %>% 
       ungroup() %>% 
       filter(funding_agency == "USAID") %>% 
-      rename_all(~str_replace(., "cop20", "cop20_usaid")) %>% 
+      pivot_wider(names_from = planning_cycle, 
+                  names_glue = "{planning_cycle}_{.value}",
+                  values_from = c(budget, budget_sans_ghsc, share, share_sans_ghsc)) %>% 
+      rename_all(~str_replace(., "cop(19|20)", "cop\\1_usaid")) %>% 
       rename(operatingunit = operating_unit) %>% 
       select(-funding_agency)
     
@@ -55,6 +65,38 @@
     df_budget <- df_budget %>% 
       filter(str_detect(operatingunit, "Region", negate = TRUE)) %>% 
       rename(countryname = operatingunit)
+    
+    
+## GH Budget
+  #make names usable
+    df_ghbudget <- clean_names(df_ghbudget)
+    
+    df_ghbudget <- df_ghbudget %>% 
+      select(countryname = bureau_program, ends_with("total_allocation"), hiv_aids) %>% 
+      rowwise() %>% 
+      mutate(fy20_usaid_ghbudget = sum(ghp_state_fy_2020_total_allocation, 
+                                       ghp_usaid_fy_2020_total_allocation, na.rm = TRUE) - sum(hiv_aids, na.rm = TRUE)) %>% 
+      ungroup() %>% 
+      select(countryname, fy20_usaid_ghbudget) %>% 
+      mutate(fy20_usaid_ghbudget = fy20_usaid_ghbudget * 1000)
+    
+#merge
+    df_budget <- df_budget %>% 
+      left_join(df_ghbudget)
+    
+    
+    df_budget <- df_budget %>% 
+      relocate(iso, .after = "countryname")
+    
+    # df_budget %>% 
+    #   select(countryname, cop19_usaid_budget, fy20_usaid_ghbudget) %>%
+    #   mutate(countryname = fct_reorder(countryname, fy20_usaid_ghbudget)) %>% 
+    #   ggplot(aes(y = countryname)) +
+    #   geom_col(aes(x = fy20_usaid_ghbudget/1000000), fill = "gray") +
+    #   geom_col(aes(x = cop19_usaid_budget/1000000), fill = "black") +
+    #   scale_x_continuous(label = scales::comma) +
+    #   labs(x = "FY20 Budget (millions USD)", y = NULL) 
+    #   prinf()
 
 # EXPORT ------------------------------------------------------------------
 
